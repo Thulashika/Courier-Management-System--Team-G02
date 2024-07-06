045645490221c71437b9016bb6ffa872b0f0c47b
@cyber24 <lasibala24@gmail.com>
2024-07-06 13:15:53 +0530

    modify some files

--- frontend/src/views/pages/login/Login.js
+++ frontend/src/views/pages/login/Login.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import {
   CButton,
   CCard,
@@ -15,8 +15,54 @@ import {
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { cilLockLocked, cilUser } from '@coreui/icons'
+import axios from 'axios'
+
+function useQuery() {
+  const {search} = useLocation()
+  return React.useMemo (() => new URLSearchParams(search), [search])
+}
 
 const Login = () => {
+
+  const [user, setUser] = useState({
+    email:'',
+    password:''
+  })
+
+  const query = useQuery()
+
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e)=> {
+    e.preventDefault()
+
+    axios(`http://localhost:6431/userLogin/${query.get('id')}`, {
+      data: user,
+      method: 'POST'
+    }).then(res => {
+      if(res.ok){
+        console.log(res.json)
+        navigate('/')
+      }
+      else {
+        console.error(res.json.message)
+        // alert("Not Login successfully")
+      }
+    }).catch(err => {
+      alert("Not Login successfully")
+    })
+
+    // const result = response.json();
+    // if (response.ok) {
+    //   // Handle successful login, e.g., store token
+    //   console.log(result);
+    // } else {
+    //   // Handle login error
+    //   console.error(result.message);
+    // }
+  }
+
   return (
     <div className="bg-body-tertiary min-vh-100 d-flex flex-row align-items-center">
       <CContainer>
@@ -24,17 +70,23 @@ const Login = () => {
           <CCol md={8}>
             <CCardGroup>
               <CCard className="p-4">
-                
                 <CCardBody>
-                  <CForm>
+                  <CForm onSubmit={handleSubmit}>
                     <h1>Login</h1>
                     <p className="text-body-secondary">Sign In to your account</p>
                     <CInputGroup className="mb-3">
+                      <CInputGroupText>@</CInputGroupText>
+                      <CFormInput 
+                        type='email' 
+                        placeholder="Email" 
+                        autoComplete="email" 
+                        required
+                        onChange={(e) => setUser({...user, email:e.target.value})}
+                        />
                       {/* <CInputGroupText>
                         <CIcon icon={cilUser} />
-                      </CInputGroupText> */}
-                      <CInputGroupText>@</CInputGroupText>
-                      <CFormInput placeholder="Email" autoComplete="email" />
+                      </CInputGroupText>
+                      <CFormInput type='text' placeholder="User name" autoComplete="userName" required/> */}
                     </CInputGroup>
                     <CInputGroup className="mb-4">
                       <CInputGroupText>
@@ -44,11 +96,13 @@ const Login = () => {
                         type="password"
                         placeholder="Password"
                         autoComplete="current-password"
+                        required
+                        onChange={(e) => setUser({...user, password:e.target.value})}
                       />
                     </CInputGroup>
                     <CRow>
                       <CCol xs={6}>
-                        <CButton color="primary" className="px-4">
+                        <CButton type='submit' color="primary" className="px-4">
                           Login
                         </CButton>
                       </CCol>
